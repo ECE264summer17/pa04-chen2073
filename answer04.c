a3012752f7f9df160fc085239068f63c6fb53971
Hao Chen <chen2073@ecegrid-thin7.ecn.purdue.edu>
2017-06-29 18:56:34 -0400

    answer04.c

--- answer04.c
+++ answer04.c
@@ -20,7 +20,19 @@
 
 char **Allocate_maze_space(int nrow, int ncol)
 {
-   return NULL;
+	char ** arry = (char**) calloc(nrow,  sizeof(char*));
+
+	if(arry == NULL){
+		return NULL;
+	}
+	
+	
+	for(int i=0; i< nrow; i++){
+		arry[i] = (char*) calloc(ncol, sizeof(char));
+	}
+	
+	return arry;
+
 }
 
 // free the memory used for the maze
@@ -28,7 +40,13 @@ char **Allocate_maze_space(int nrow, int ncol)
 
 void Deallocate_maze_space(char **maze, int nrow) 
 {
-   return;
+	int i;
+	
+	for(i=0; i < nrow; i++){
+	free(maze[i]);}
+
+	free(maze);
+   
 }
 
 #endif /* NTEST_MEM */
@@ -41,7 +59,45 @@ void Deallocate_maze_space(char **maze, int nrow)
 char **Read_maze_from_2Dfile(FILE *fptr, int *nrow, int *ncol)
 {
    *nrow = *ncol = 0;
-   return NULL;
+
+	char ** maze;
+	
+	fseek(fptr, 0, SEEK_SET);
+
+	int f = fgetc(fptr);
+
+	while(f != EOF){
+		if(f == '\n'){
+			(*nrow)++;
+		}
+		else if (*nrow == 0){
+			(*ncol)++;
+		}
+	f = fgetc(fptr);
+	}
+
+	//int length = ftell(fptr);
+	//(*ncol) = ((length - *nrow) / (*nrow));
+
+	maze = Allocate_maze_space(*nrow, *ncol);
+
+	fseek(fptr, 0, SEEK_SET);
+
+	int i, j;
+
+	//f = 0;
+
+	for(i=0; i < *nrow; i++){
+	    for (j=0; j <= *ncol; j++){	
+			//f = fgetc(fptr);
+		if(f != '\n'){
+		   	maze[i][j] = fgetc(fptr);}
+		
+
+	     }
+	}
+
+	return maze;
 }
 
 #endif /* NTEST_READ */
@@ -53,7 +109,26 @@ char **Read_maze_from_2Dfile(FILE *fptr, int *nrow, int *ncol)
 
 int Write_maze_to_2Dfile(char *filename, char **maze, int nrow, int ncol) 
 {
-   return -1;
+
+	FILE * fp = fopen(filename, "w");
+
+	if(fp == NULL){
+	return -1;}
+
+	int i, j;
+
+	for(i=0; i < nrow; i++){
+
+		for(j=0; j < ncol; j++){
+
+		fputc(maze[i][j], fp);}
+
+	fputc('\n', fp);
+	}
+
+	fclose(fp);
+
+   return (i * (j+1)); 
 }
 
 #endif /* NTEST_WRITE */
@@ -73,7 +148,63 @@ int Write_maze_to_2Dfile(char *filename, char **maze, int nrow, int ncol)
 char **Expand_maze_row(char **maze, int nrow, int ncol, int *rrow, int *rcol)
 {
    *rrow = *rcol = 0;
-   return NULL;
+   
+	char ** new_maze;
+
+	*rrow = (2 * nrow) - 1;
+	*rcol = ncol;
+
+	new_maze = Allocate_maze_space(*rrow, *rcol);
+
+	int i, j;
+
+	/*for(i=0; i < (*nrrow); i++){
+
+		for(j=0; j < (*rcol); j++){
+	
+			if( (i+1) * (j+1) == sizeof(maze) ){             //check end of maze
+			new_maze[i][j] = maze[(*rrow+1) - i][j];}   
+
+			new_maze[i][j] = maze[i][j];
+		}
+	}*/
+
+	for(i=0; i < nrow; i++){
+
+		for(j=0; j < ncol; j++){
+
+			new_maze[i][j] = maze[i][j];
+		}
+	}
+	int k=2;
+	for(i=nrow; i < *rrow; i++){
+
+		for(j=0; j < *rcol; j++){
+
+			new_maze[i][j] = maze[nrow - k][j];
+		}
+	k++;
+	}
+
+
+
+
+
+
+
+
+
+	// /* for checking output purpose
+	for(i=0; i < *rrow; i++){
+		 for(j=0; j < *rcol; j++){
+			printf("%c", new_maze[i][j]);
+		}
+	printf("\n");
+	}
+	printf("\n");
+	// */ comment out after checking
+
+	return new_maze;
 }
 
 #endif /* NTEST_ROW */
